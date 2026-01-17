56a1cdbd76cc287820fbfe967074cd4763a5d28f
Diego <diegoandresvv@gmail.com>
2026-01-17 18:45:10 +0100

    hotfix: corregir rutas de imágenes en initialCards
    Se actualizaron las rutas de '../images/' a './images/' para asegurar que las tarjetas carguen correctamente desde el directorio raíz.

--- scripts/index.js
+++ scripts/index.js
@@ -8,32 +8,32 @@ let initialCards = [
   {
     name: "Valle de Yosemite",
     //link: "https://tripleten-content.s3.us-west-1.amazonaws.com/web-code/moved_yosemite.jpg"
-    link: "../images/valle-de-yosemite.jpg"
+    link: "./images/valle-de-yosemite.jpg"
   },
   {
     name: "Lago Louise",
     //link: "https://tripleten-content.s3.us-west-1.amazonaws.com/web-code/moved_lake-louise.jpg"
-    link: "../images/lago-louise.jpg"
+    link: "./images/lago-louise.jpg"
   },
   {
     name: "Montañas Calvas",
     //link: "https://tripleten-content.s3.us-west-1.amazonaws.com/web-code/moved_bald-mountains.jpg"
-    link: "../images/montanas-calvas.jpg"
+    link: "./images/montanas-calvas.jpg"
   },
   {
     name: "Latemar",
     //link: "https://tripleten-content.s3.us-west-1.amazonaws.com/web-code/moved_latemar.jpg"
-    link: "../images/latemar.jpg"
+    link: "./images/latemar.jpg"
   },
   {
     name: "Parque Nacional de la Vanoise",
     //link: "https://tripleten-content.s3.us-west-1.amazonaws.com/web-code/moved_vanoise.jpg"
-    link: "../images/vanoise-national-park.jpg"
+    link: "./images/vanoise-national-park.jpg"
   },
   {
     name: "Lago di Braies",
     //link: "https://tripleten-content.s3.us-west-1.amazonaws.com/web-code/moved_lago.jpg"
-    link: "../images/lago-di-braies.jpg"
+    link: "./images/lago-di-braies.jpg"
   }
 ];
 // Seleccion de todos los popups de la pagina
